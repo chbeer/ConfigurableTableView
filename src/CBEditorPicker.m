972fc66492361a6a7e58261bb3c2fef0310cca63
Christian Beer <christian.beer@chbeer.de>
2014-09-03 11:46:48 +0200

    Fixes crash in CBEditorPicker because of faulty memory management

--- src/CBEditorPicker.m
+++ src/CBEditorPicker.m
@@ -13,6 +13,9 @@
 #import "CBOptionsPickerController.h"
 
 @implementation CBEditorPicker
+{
+    CBOptionsPickerController *_pickerController;
+}
 
 - (id) initWithOptions:(NSArray*)options {
 	if (self = [super init]) {
@@ -26,14 +29,14 @@
 	return editor;
 }
 
-- (void) openEditorForCell:(CBCell*)cell inController:(CBConfigurableTableViewController*)ctrl {
-	CBOptionsPickerController *pc = [[CBOptionsPickerController alloc] initWithOptions:_options frame:ctrl.view.bounds];
-	[pc setSelectedValue:[ctrl valueForCell:cell]];
-	[pc openEditorForCell:cell inController:ctrl];
+- (void) openEditorForCell:(CBCell*)cell inController:(CBConfigurableTableViewController*)ctrl
+{
+	_pickerController = [[CBOptionsPickerController alloc] initWithOptions:_options frame:ctrl.view.bounds];
+	[_pickerController setSelectedValue:[ctrl valueForCell:cell]];
+	[_pickerController openEditorForCell:cell inController:ctrl];
     [ctrl.tableView scrollToRowAtIndexPath:[ctrl.model indexPathOfCell:cell]
                           atScrollPosition:UITableViewScrollPositionMiddle
                                   animated:YES];
 }
-
 
 @end
