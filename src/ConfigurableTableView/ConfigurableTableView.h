b8ae9b4d7f5bbef4697d44444d92e6772de69e3a
Christian Beer <christian.beer@chbeer.de>
2015-10-14 23:46:17 +0200

    Added shared dynamic framework for Carthage support

---
+++ src/ConfigurableTableView/ConfigurableTableView.h
@@ -0,0 +1,19 @@
+//
+//  ConfigurableTableView.h
+//  ConfigurableTableView
+//
+//  Created by Christian Beer on 14.10.15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConfigurableTableView.
+FOUNDATION_EXPORT double ConfigurableTableViewVersionNumber;
+
+//! Project version string for ConfigurableTableView.
+FOUNDATION_EXPORT const unsigned char ConfigurableTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConfigurableTableView/PublicHeader.h>
+
+
