5a0a592697a5a3ef934850f7220ce1be6143a554
Christian Beer <christian.beer@chbeer.de>
2015-10-20 06:00:37 +0200

    Renamed Framework

--- src/CBConfigurableTableView.h
+++ src/CBConfigurableTableView.h
@@ -6,7 +6,15 @@
 //  Copyright 2010 Christian Beer. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConfigurableTableView.
+FOUNDATION_EXPORT double ConfigurableTableViewVersionNumber;
+
+//! Project version string for ConfigurableTableView.
+FOUNDATION_EXPORT const unsigned char ConfigurableTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConfigurableTableView/PublicHeader.h>
 
 #import "CBConfigurableTableViewController.h"
 
